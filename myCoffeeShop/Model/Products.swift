5f6993dbc087802d639c086090c7d5a4d5c3c95a
Vasily Shuklin <b88bb@yandex.ru>
2025-01-22 19:53:43 +0500

    Update Products

--- myCoffeeShop/Model/Products.swift
+++ myCoffeeShop/Model/Products.swift
@@ -3,17 +3,28 @@ import SwiftUI
 
 let espressoWithImage = Product(
     name: "Espresso",
-    price: 5.99,
-    description: "• Made from expertly roasted, high-quality Arabica beans • Brewed to perfection using our state-of-the-art espresso machine • Rich, bold flavors with notes of chocolate and caramel",
+    price: 150,
+    description: """
+    • Made from expertly roasted, high-quality Arabica beans
+    • Brewed to perfection using our state-of-the-art espresso machine
+    • Rich, bold flavors with notes of chocolate and caramel
+    """,
     image: ImageResource(name: "espresso", bundle: .main)
 )
 
 let greenTea = Product(
     name: "Green tea",
-    price: 6.99,
-    description: "• Light, crisp flavor with subtle grassy notes • High in antioxidants and nutrients • Low in caffeine compared to other teas • Made from young tea leaves that are quickly heated to preserve freshness • Can be enjoyed hot or chilled for a refreshing iced tea",
+    price: 200,
+    description: """
+    • Light, crisp flavor with subtle grassy notes
+    • High in antioxidants and nutrients
+    • Low in caffeine compared to other teas
+    • Made from young tea leaves that are quickly heated to preserve freshness
+    • Can be enjoyed hot or chilled for a refreshing iced tea
+    """,
     image: ImageResource(name: "greentea", bundle: .main)
 )
+
 let products = [
     espressoWithImage,
     greenTea,
