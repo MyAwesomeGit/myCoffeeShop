2490dc5303e8cadfa8b30666a75d2295156f37ea
Vasily Shuklin <b88bb@yandex.ru>
2025-01-22 19:53:02 +0500

    Update ProductDetail

--- myCoffeeShop/View/ProductDetail.swift
+++ myCoffeeShop/View/ProductDetail.swift
@@ -2,35 +2,44 @@ import SwiftUI
 
 struct ProductDetail: View {
     let product: Product
+    @EnvironmentObject var cart: CartManager
     
     var body: some View {
-        VStack {
-            Text("Product Details")
-                .font(.largeTitle)
-            
-            Text("Name: \(product.name)")
-            Text("Price: $\(product.price)")
-            Text("Description: \(product.description)")
-            
-            // Display the image if product.image is not nil
-            if let image = product.image {
-                Image(image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
+        ZStack {
+            Color(mainBackgroundColor)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("\(product.name)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                Text("\(formatPrice(price: product.price)) ₽")
+                    .font(.title)
+                    .foregroundColor(.green)
+                if let image = product.image {
+                    Image(image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
+                Spacer()
+                Text("\(product.description)")
+                    .font(.custom("San Francisco", fixedSize: 20))
+                    .foregroundColor(.white)
+                
+                // Add "Add to Cart" button
+                Button(action: {
+                    print(cart)
+                    cart.addToCart(product: product)
+                }) {
+                    Text("Add to Cart")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                Spacer()
             }
-            
-            Spacer()
-
+            .padding()
         }
-        .padding()
     }
 }
-
-
-
-struct ProductDetail_Previews: PreviewProvider {
-    static var previews: some View {
-        ProductDetail(product: greenTea)
-    }
-}
-
